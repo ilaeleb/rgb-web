2bb10c160bef2deb1c23f9c693a0654ede58008c
ilaeleb <ilariaebrunetti@gmail.com>
2026-02-23 15:44:46 +0100

    initial commit

---
+++ rgb-backend/src/main/java/com/magnolia/rgb_backend/RgbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.magnolia.rgb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgbBackendApplication.class, args);
+	}
+
+}
---
+++ rgb-backend/src/test/java/com/magnolia/rgb_backend/RgbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnolia.rgb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
